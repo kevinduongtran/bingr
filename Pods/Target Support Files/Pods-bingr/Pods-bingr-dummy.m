175603a70a8e80d4a25475088a838e42f76c4ec6
kevinduongtran <kevinduongtran@gmail.com>
2016-04-22 06:12:56 -0700

    init iOS Project, installed Pods, installed alamoFire, installed swiftyJSON

---
+++ Pods/Target Support Files/Pods-bingr/Pods-bingr-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_bingr : NSObject
+@end
+@implementation PodsDummy_Pods_bingr
+@end
---
+++ bingr/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  bingr
+//
+//  Created by Kevin Duong-Tran on 4/22/16.
+//  Copyright © 2016 Kevin Duong-Tran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
